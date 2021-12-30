69d2891f0ba7eadabd93369677195f444970ef44
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-12-30 20:08:20 +0100

    Finish reach Router.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import {NavLink, Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
             <div className="container">
                 <Link to="/" className="fs-3 ubuntu navbar-brand">Rick & Morty
                     <span style={{
@@ -37,7 +37,7 @@ const Navbar = () => {
                     className="collapse navbar-collapse justify-content-end"
                     id="navbarNavAltMarkup">
                     <div className="navbar-nav fs-5">
-                        <NavLink to="/" className="nav-link">Characters</NavLink>
+                        <NavLink activeClassName="active" to="/" className="nav-link">Characters</NavLink>
                         <NavLink to="/episodes" className="nav-link">Episodes</NavLink>
                         <NavLink to="/location" className="nav-link">Location</NavLink>
                     </div>
