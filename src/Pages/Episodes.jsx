def133b97c863f3af8770eae654e3cbad0017174
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-01-01 18:03:30 +0100

    Use useState and usEffect hooks to create Episode page for episodes end.

--- src/Pages/Episodes.jsx
+++ src/Pages/Episodes.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const Episodes = () => {
+    let [id, setID] = useState(1);
+    let [info, setInfo] = useState([]);
+    // Destructuring the info we get from episodes end for episodes route
+    let {air_date, name} = info;
+    let api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+    useEffect(()=>{
+        (async function() {
+            let data = await fetch(api).then((res)=>{return res.json()});
+            setInfo(data);
+        })();
+
+    }, [api]);
+
     return (
-        <div>
-            Episodes ...
+        <div className = "container">
+        <div className = "row">
+            <h1 className="text-center mb-3">Episode: {name}</h1>
+            <h5 className="text-center">Air Date {air_date}</h5>
+        </div>
+        <div className = "row"></div>   
         </div>
     );
 }
